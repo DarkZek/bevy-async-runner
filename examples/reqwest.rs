37d0de7c6f3a5450e3e106f9db80f09f684af566
Marshall Scott <masheddarkzek@gmail.com>
2025-04-29 12:55:44 -0600

    feat: Add new example for Reqwest

---
+++ examples/reqwest.rs
@@ -0,0 +1,56 @@
+use std::time::Duration;
+use bevy::prelude::*;
+use bevy_async_runner::{AsyncRunner, AsyncRunnerPlugin};
+use serde::Deserialize;
+use tokio::time::sleep;
+
+#[cfg(not(any(feature = "tokio-runtime", feature = "tokio-runtime-multi-thread")))]
+compile_error!("Tokio feature required to use `reqwest`. Try `--features=\"tokio-runtime-multi-thread\"");
+
+#[cfg(all(feature = "tokio-runtime", not(target_arch = "wasm32")))]
+compile_error!("Tokio runtime will be blocked when running Bevy so will not work on non wasm targets");
+
+pub fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugins(AsyncRunnerPlugin)
+        .insert_resource(IpAddress(String::default()))
+        .insert_resource(Time::<Fixed>::from_hz(4.0))
+        .add_systems(Startup, fetch_ip)
+        .add_systems(FixedUpdate, print_ip)
+        .run();
+}
+
+#[derive(Resource)]
+struct IpAddress(String);
+
+fn fetch_ip(runner: Res<AsyncRunner>) {
+    runner.schedule(load_name(), |In(name): In<Result<String>>, mut stored_ip: ResMut<IpAddress>| {
+        stored_ip.0 = name.unwrap();
+    });
+}
+
+#[derive(Deserialize)]
+struct IpifyResponse {
+    ip: String
+}
+
+async fn load_name() -> Result<String> {
+
+    sleep(Duration::from_secs_f64(1.0)).await;
+
+    let response = reqwest::Client::new()
+        .get("https://api.ipify.org?format=json")
+        .send()
+        .await?;
+
+    let data = response
+        .json::<IpifyResponse>()
+        .await?;
+
+    Ok(data.ip)
+}
+
+fn print_ip(res: Res<IpAddress>) {
+    info!("Ip Address: {}", res.0);
+}
